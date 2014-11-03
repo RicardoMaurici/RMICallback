0811af441bfd69853767fea660716e522f5af0e6
Ricardo Maurici <ricardo.maurici@hotmail.com>
2014-11-03 21:30:27 -0200

    teste

--- src/CallBack.java
+++ src/CallBack.java
@@ -2,6 +2,7 @@
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
+
 public interface CallBack extends Remote {
 
     public String programar(Cliente cliente, Long l) throws RemoteException;
